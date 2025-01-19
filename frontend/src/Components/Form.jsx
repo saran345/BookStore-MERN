d5283c7a7a40ccbdc4c15253543252d01cd1c11d
saran345 <saran65444@gmail.com>
2025-01-19 23:11:51 +0530

    .

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,12 +1,18 @@
-
+import Api from "./Data/Api"
+import { useState } from "react"
 export default function Form() {
- 
-   const [ formData, setFormData ] = useState([])
+
+   const [ formData, setFormData ] = useState({})
+
+  const handleChange=()=>{
+        
+  }
 
 
-
-     const handleChange=()=>{
-       
+     const handleSubmit=async()=>{
+        const response=await Api.postContact("/tell/contact",formData)
+        setFormData(response.data)
+        console.log("SuccessFully Send!",response.data)
      }
 
   return (
@@ -15,29 +21,29 @@ export default function Form() {
       <h1 className="font-sans text-[2em] text-baseline mb-6 text-gray-800">Let&apos;s Talk</h1>
       <p className="text-lg">Have Questions? We’d Love to Hear from You!</p>
          <hr className="h-px my-8  bg-gray-200 border-0 dark:bg-gray-700 " />
-        <form className="grid gap-5">
+        <form className="grid gap-5" onSubmit={handleSubmit}>
          <div>
            <label className=" text-lg font-medium text-gray-700">name:</label>
-           <input type="text" name="text" value={formData.name} onChange={handleChange}
+           <input type="text" name="text" value={formData.name}  onChange={e=> setFormData(e.target)}
            className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-sky-500 focus:border-sky-500"  />
          </div>
          <div>
            <label className=" text-lg font-medium text-gray-700">email:</label>
-           <input type="email" name="text" value={formData.email} onChange={handleChange}
+           <input type="email" name="text" value={formData.email} onChange={e=> setFormData(e.target)}
             className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-sky-500 focus:border-sky-500"   />
          </div>
          <div>
            <label className=" text-lg font-medium text-gray-700">Subject:</label>
-           <input type="text" name="text" value={formData.subject} onChange={handleChange}
+           <input type="text" name="text" value={formData.subject} onChange={e=> setFormData(e.target)}
             className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-sky-500 focus:border-sky-500"   />
          </div>
          <div>
          <label className=" text-lg font-medium text-gray-700">Message:</label>
-         <textarea  name="text"  rows={4} value={formData.message}
+         <textarea  name="text"  rows={4} value={formData.message} onChange={e=> setFormData(e.target)}
           className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-sky-500 focus:border-sky-500"
          />
          </div>
-         <button type="button" className="w-full py-2 px-4 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 focus:outline-none">Send Message </button>
+         <button type="submit" className="w-full py-2 px-4 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 focus:outline-none">Send Message </button>
         </form>
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
          <h1 className="font relative text-[2em]">Follow Me</h1>
@@ -53,7 +59,7 @@ export default function Form() {
         </button>
          <button>
          <svg className="h-8 w-8 text-blue-600"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
-                  <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" /> 
+                  <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />
               <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" /></svg>
          </button>
          </div>
